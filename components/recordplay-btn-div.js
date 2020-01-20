8ff9fa95b026faa30257617344146ab7966d68ea
bnidevs <bill.8ni@gmail.com>
2020-01-20 15:18:29 -0500

    2 files modified

--- components/recordplay-btn-div.js
+++ components/recordplay-btn-div.js
@@ -4,7 +4,7 @@ export default ({ name }) =>
 
 <div id="recordplay-btn-div">
 
-<button id="record-btn"><i id="micicon" class="fa fa-microphone"></i></button>
-<button id="play-btn" disabled><i id="plyicon" class="fa fa-play"></i></button>
+<button id="record-btn"><i id="micicon" className="fa fa-microphone"></i></button>
+<button id="play-btn" disabled><i id="plyicon" className="fa fa-play"></i></button>
 
 </div>;
